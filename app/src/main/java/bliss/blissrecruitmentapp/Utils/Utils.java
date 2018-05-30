fd3fdd549bf219833aa70fddf50b84f3f22867bd
lluismmartins7@gmail.com <lluismmartins7@gmail.com>
2018-05-30 17:04:46 +0100

    Added email validation to share activity

--- app/src/main/java/bliss/blissrecruitmentapp/Utils/Utils.java
+++ app/src/main/java/bliss/blissrecruitmentapp/Utils/Utils.java
@@ -4,4 +4,11 @@ public class Utils {
     public static String API_BASE_URL = "https://private-bbbe9-blissrecruitmentapi.apiary-mock.com";
     public static String APP_BASE_LINK = "blissrecruitment://";
 
+    public static String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`" +
+            "{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01" +
+            "-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](" +
+            "?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]" +
+            "|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\" +
+            "x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
 }
--- app/src/main/java/bliss/blissrecruitmentapp/view/ShareActivity.java
+++ app/src/main/java/bliss/blissrecruitmentapp/view/ShareActivity.java
@@ -2,11 +2,14 @@ package bliss.blissrecruitmentapp.view;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 import bliss.blissrecruitmentapp.R;
 import bliss.blissrecruitmentapp.databinding.ActivityShareBinding;
@@ -14,6 +17,8 @@ import bliss.blissrecruitmentapp.repository.ShareRepository;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+
+import static bliss.blissrecruitmentapp.Utils.Utils.EMAIL_REGEX;
 
 public class ShareActivity extends AppCompatActivity {
     private ActivityShareBinding mBinding;
@@ -37,8 +42,19 @@ public class ShareActivity extends AppCompatActivity {
 
     }
 
+    public boolean isEmailValid(String email) {
+        return Pattern.compile(EMAIL_REGEX).matcher(email).matches();
+    }
+
     public void shareLink(View view){
         String email = mBinding.viewActivityShareMailEdittext.getText().toString();
+
+        if(!isEmailValid(email)) {
+            Toast.makeText(mContext,mContext.getString(R.string.lbl_share_activity_invalid_email),Toast.LENGTH_SHORT).show();
+            return;
+        }
+        
+
         Disposable disposable = mRepository.shareApp(email, shareUrl)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
